204337949d4f368b05845e3ecc1ca9676b35e186
marcuspramirez <marcus.p.ramirez2020@gmail.com>
2020-09-22 12:23:30 -0500

    updated server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const fs = require("fs");
 const htmlRouter = require('./htmlRoutes');
 const apiRouter = require('./apiRoutes');
 const app = express();
 
 const PORT = process.env.PORT;
+// route the sends the user to notes.html
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 
 app.use(htmlRouter);
 app.use('/api',apiRouter);
